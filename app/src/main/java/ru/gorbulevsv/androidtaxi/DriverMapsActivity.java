dab2297c0ec7ef6392d432511deca08e94318507
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-07-29 00:42:42 +0300

    1

--- app/src/main/java/ru/gorbulevsv/androidtaxi/DriverMapsActivity.java
+++ app/src/main/java/ru/gorbulevsv/androidtaxi/DriverMapsActivity.java
@@ -23,7 +23,7 @@ public class DriverMapsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver_maps);
 
         toolbar = findViewById(R.id.toolbarDriverMaps);
-        toolbar.setTitle("Карта водителя");
+        toolbar.setTitle("Карта водителя, привет");
         setSupportActionBar(toolbar);
 
         mapview = findViewById(R.id.driverMaps);
