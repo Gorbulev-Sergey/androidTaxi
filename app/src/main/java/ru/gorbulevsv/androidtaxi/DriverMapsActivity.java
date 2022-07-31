66c1ba3523d296a6bbc874f33824f36f1aa17a6c
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-07-31 23:48:22 +0300

    2

--- app/src/main/java/ru/gorbulevsv/androidtaxi/DriverMapsActivity.java
+++ app/src/main/java/ru/gorbulevsv/androidtaxi/DriverMapsActivity.java
@@ -1,19 +1,41 @@
 package ru.gorbulevsv.androidtaxi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.location.Location;
+import com.yandex.mapkit.location.LocationListener;
+import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
 
-public class DriverMapsActivity extends AppCompatActivity {
+public class DriverMapsActivity extends AppCompatActivity implements UserLocationObjectListener {
     Toolbar toolbar;
-    private MapView mapview;
+    TextView textDriverLat, textDriverLon;
+    private MapView mapView;
+    private UserLocationLayer userLocationLayer;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +47,45 @@ public class DriverMapsActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbarDriverMaps);
         toolbar.setTitle("Карта водителя");
         setSupportActionBar(toolbar);
+        textDriverLat = findViewById(R.id.textDriverLat);
+        textDriverLon = findViewById(R.id.textDriverLan);
 
-        mapview = findViewById(R.id.driverMaps);
-        mapview.getMap().move(
+        mapView = findViewById(R.id.driverMaps);
+        mapView.getMap().move(
                 new CameraPosition(new Point(54.516011, 36.246966), 14.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 5),
                 null);
+
+        requestLocationPermission();
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        mapKit.resetLocationManagerToDefault();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+
+        mapKit.createLocationManager().requestSingleUpdate(new LocationListener() {
+            @Override
+            public void onLocationUpdated(@NonNull Location location) {
+                textDriverLat.setText(String.valueOf(location.getPosition().getLatitude()));
+                textDriverLon.setText(String.valueOf(location.getPosition().getLongitude()));
+                mapView.getMap().move(
+                        new CameraPosition(location.getPosition(), 14.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 1),
+                        null);
+            }
+
+            @Override
+            public void onLocationStatusUpdated(@NonNull LocationStatus locationStatus) {
+
+            }
+        });
     }
 
     @Override
     protected void onStop() {
-        mapview.onStop();
+        mapView.onStop();
         MapKitFactory.getInstance().onStop();
         super.onStop();
     }
@@ -44,6 +94,58 @@ public class DriverMapsActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         MapKitFactory.getInstance().onStart();
-        mapview.onStart();
+        mapView.onStart();
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                "android.permission.ACCESS_FINE_LOCATION")
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{"android.permission.ACCESS_FINE_LOCATION"},
+                    PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+
+        pinIcon.setIcon(
+                "icon",
+                ImageProvider.fromResource(this, R.drawable.icon),
+                new IconStyle().setAnchor(new PointF(0f, 0f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(0f)
+                        .setScale(1f)
+        );
+
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView) {
+
+    }
+
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent) {
+
     }
 }
